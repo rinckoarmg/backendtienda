08376d9324086c94da403b218fc2c8a80dfee38b
rinckoar <rinckoar@gmail.com>
2021-09-22 17:27:46 -0500

    controladores productos y proveedores

--- src/main/java/com/backend/tienda/controladores/ProductosControlador.java
+++ src/main/java/com/backend/tienda/controladores/ProductosControlador.java
@@ -1,4 +1,60 @@
 package com.backend.tienda.controladores;
 
+import com.backend.tienda.entidades.Productos;
+import com.backend.tienda.servicios.ProductosServicios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
 public class ProductosControlador {
+
+    @Autowired
+    private ProductosServicios productosServicios;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Productos>> listaProductos(){
+        List<Productos> productos = productosServicios.listarProductos();
+        if (productos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(productos);
+        }
+    }
+
+    @GetMapping("/{codigo_producto}")
+    public ResponseEntity<Productos> buscarProductos(@PathVariable("codigo_producto") Double codigo_producto) {
+        Productos productos = productosServicios.buscarProducto(codigo_producto);
+        if (productos == null){
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(productos);
+        }
+    }
+
+    @PostMapping("/guardar")
+    public ResponseEntity<Productos> guardarproducto(@RequestBody Productos productos) {
+        Productos nuevo_producto = productosServicios.guardarProductos(productos);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevo_producto);
+    }
+
+    @PutMapping("/actualizar/{codigo_producto}")
+    public ResponseEntity<Productos> actualizarProductos(@PathVariable("codigo_producto") Double codigo_producto, @RequestBody Productos productos){
+        productos.setCodigo_producto(codigo_producto);
+        Productos productoDB = productosServicios.actualizarProductos(productos);
+        if (productoDB == null){
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(productoDB);
+        }
+    }
+
+    @DeleteMapping("/eliminar/{codigo_producto}")
+    public void eliminarProducto (@PathVariable("codigo_producto") Double codigo_producto){
+        productosServicios.eliminarProductos(codigo_producto);
+    }
 }
--- src/main/java/com/backend/tienda/controladores/ProveedoresControlador.java
+++ src/main/java/com/backend/tienda/controladores/ProveedoresControlador.java
@@ -1,4 +1,58 @@
 package com.backend.tienda.controladores;
 
+import com.backend.tienda.entidades.Proveedores;
+import com.backend.tienda.servicios.ProveedoresServicios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class ProveedoresControlador {
+
+    @Autowired
+    private ProveedoresServicios proveedoresServicios;
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Proveedores>> listaProveedores(){
+        List<Proveedores> proveedores = proveedoresServicios.listarProveedores();
+        if (proveedores.isEmpty()){
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(proveedores);
+        }
+    }
+
+    @GetMapping("/{nitproveedor}")
+    public ResponseEntity<Proveedores> buscarProveedor(@PathVariable("nitproveedor") Double nitproveedor){
+        Proveedores proveedores = proveedoresServicios.buscarProveedor(nitproveedor);
+        if (proveedores == null){
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(proveedores);
+        }
+    }
+
+    @PostMapping("/guardar")
+    public ResponseEntity<Proveedores> guardarProveedor(@RequestBody Proveedores proveedores){
+        Proveedores nuevo_proveedor = proveedoresServicios.guardarProveedores(proveedores);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevo_proveedor);
+    }
+
+    @PutMapping("/actualiar/{nitproveedor}")
+    public ResponseEntity<Proveedores> actualizarProveedores(@PathVariable("nitproveedores") Double nitproveedor, @RequestBody Proveedores proveedores){
+        proveedores.setNitproveedor(nitproveedor);
+        Proveedores proveedorBD = proveedoresServicios.actualizarProveedores(proveedores);
+        if (proveedorBD == null){
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(proveedorBD);
+        }
+    }
+
+    @DeleteMapping("/eliminar/{nitproveedor}")
+    public void eliminarProveedor (@PathVariable("nitproveedor") Double nitproveedor){
+        proveedoresServicios.eliminarProveedores(nitproveedor);
+    }
 }
